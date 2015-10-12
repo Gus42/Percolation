bb8aadbd8c39f884f9dac846e088e482391ce605
Gus42 <gus815@gmail.com>
2015-10-12 11:57:06 +0100

    Fix: indexes
    Fixed the indexes. Now the grid is N * N from 0 to N-1, but when
    somebody calls methods he should use like from 1 to N

--- Percolation.java
+++ Percolation.java
@@ -5,77 +5,79 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
   boolean grid[][];
   int N;
+  int sites;
   WeightedQuickUnionUF ali;
-  
+
   // create N-by-N grid, with all sites blocked
-  public Percolation(int n) {
-    N = n * n + 2;
-    ali = new WeightedQuickUnionUF(N*N+2);
-    System.out.println("test1");
-    grid = new boolean[N+1][N+1];
-    for(int i=1; i <= N; i++)
-      for(int j=1; j <= N; j++)
+  public Percolation(int N) {
+    this.N = N;
+    sites = N * N + 2;
+    ali = new WeightedQuickUnionUF(sites);
+    grid = new boolean[N][N];
+    for(int i=0; i < N; i++)
+      for(int j=0; j < N; j++)
         grid[i][j] = false;
-    System.out.println("test2");
   }
-  
+
   // open site (row i, column j) if it is not open already  
   public void open(int i, int j) {
+    invalidIndices(i, j);
     i--;
     j--;
-    System.out.println("test");
-    invalidIndices(i, j);
     if(isOpen(i, j)) return;
     grid[i][j] = true;
     int index = dimensionConverter(i, j);
-    //if(i == 1) {
-    if(i != 1) {
+    if(i != 0) {
       if(isOpen(i-1, j)) {
         ali.union(index,dimensionConverter(i-1, j));
       }
+    }else{
+      ali.union(index, N*N);
     }
-    if(i != N) {
+    if(i != N-1) {
       if(isOpen(i+1, j)) {
         ali.union(index,dimensionConverter(i+1, j));
       }
+    }else{
+      ali.union(index, N*N+1);
     }
-    if(j != 1) {
+    if(j != 0) {
       if(isOpen(i, j-1)) {
         ali.union(index,dimensionConverter(i, j-1));
       }
     }
-    if(j != N) {
+    if(j != N-1) {
       if(isOpen(i, j+1)) {
         ali.union(index,dimensionConverter(i, j+1));
       }
     }
   }
-  
+
   // is site (row i, column j) open?
   public boolean isOpen(int i, int j) {
     return (grid[i--][j--] == true);
   }
-  
+
   // is site (row i, column j) full?  
   public boolean isFull(int i, int j) {
-    return p.ali.connected(p.dimensionConverter(i--, j--), p.dimensionConverter(N, N));
+    return ali.connected(dimensionConverter(i--, j--), N*N);
   }
-  
+
   // does the system percolate?  
   public boolean percolates() {
-    return true;
+    return ali.connected(N*N+1, N*N);
   }
-  
+
   // dimension converter
   public int dimensionConverter(int i, int j) {
-    return i * N - N + j;
+    return j * N + i;
   }
   // throw an exception for invalid indices
   public void invalidIndices(int i, int j) {
     if (i <= 0 || i > N) throw new IndexOutOfBoundsException("row index i out of bounds");
     if (j <= 0 || j > N) throw new IndexOutOfBoundsException("column index j out of bounds");
   }
-  
+
   // test client (optional)
   public static void main(String[] args) {
     /* 
