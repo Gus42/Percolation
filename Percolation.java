1ff92ebf07fdbd056689ae2f543b6528824c906d
Gus42 <gus815@gmail.com>
2015-10-12 16:33:30 +0100

    Fix: indexes
    Add the visualizer and fixed the indexes because it did'nt work. Now the
    code is ugly but it works well

--- Percolation.java
+++ Percolation.java
@@ -1,15 +1,14 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-  boolean grid[][];
-  int N;
-  int sites;
-  WeightedQuickUnionUF ali;
+  private boolean grid[][];
+  private int N;
+  private int sites;
+  private WeightedQuickUnionUF ali;
 
   // create N-by-N grid, with all sites blocked
   public Percolation(int N) {
+    System.out.println("test 1, I'll made a grid with dim:"+N);
     this.N = N;
     sites = N * N + 2;
     ali = new WeightedQuickUnionUF(sites);
@@ -17,36 +16,43 @@ public class Percolation {
     for(int i=0; i < N; i++)
       for(int j=0; j < N; j++)
         grid[i][j] = false;
+    System.out.println("test 2");
   }
 
   // open site (row i, column j) if it is not open already  
   public void open(int i, int j) {
+    System.out.println("test 3");
     invalidIndices(i, j);
-    i--;
-    j--;
+    System.out.println("test 4");
+    //i--;
+    //j--;
     if(isOpen(i, j)) return;
+    i--; j--;
     grid[i][j] = true;
+    i++; j++;
     int index = dimensionConverter(i, j);
-    if(i != 0) {
+    System.out.println("test 5");
+    if(i != 1) {
       if(isOpen(i-1, j)) {
         ali.union(index,dimensionConverter(i-1, j));
       }
     }else{
       ali.union(index, N*N);
     }
-    if(i != N-1) {
+    System.out.println("test 6");
+    if(i != N) {
       if(isOpen(i+1, j)) {
         ali.union(index,dimensionConverter(i+1, j));
       }
     }else{
       ali.union(index, N*N+1);
     }
-    if(j != 0) {
+    if(j != 1) {
       if(isOpen(i, j-1)) {
         ali.union(index,dimensionConverter(i, j-1));
       }
     }
-    if(j != N-1) {
+    if(j != N) {
       if(isOpen(i, j+1)) {
         ali.union(index,dimensionConverter(i, j+1));
       }
@@ -55,47 +61,41 @@ public class Percolation {
 
   // is site (row i, column j) open?
   public boolean isOpen(int i, int j) {
-    return (grid[i--][j--] == true);
+    System.out.println(i+" "+j+" is open?");
+    invalidIndices(i, j);
+    i--;
+    j--;
+    System.out.println(" return from test 11 in test isOpen. this is the value of grid i: "+i+" and j "+j+" the grid: "+grid[i][j]);
+    if(grid[i][j] == true){ System.out.println(" yes "); return true;}
+    System.out.println(" no "); return false;
   }
 
   // is site (row i, column j) full?  
   public boolean isFull(int i, int j) {
-    return ali.connected(dimensionConverter(i--, j--), N*N);
+    System.out.println("test 8");
+    //i--;
+    //j--;
+    return ali.connected(dimensionConverter(i, j), N*N);
   }
 
   // does the system percolate?  
   public boolean percolates() {
+    System.out.println("test 9");
     return ali.connected(N*N+1, N*N);
   }
 
   // dimension converter
   public int dimensionConverter(int i, int j) {
-    return j * N + i;
+    System.out.println("test 10: dimension converter with "+i+" and "+j);
+    i--;
+    j--;
+    return (i * N + j); 
   }
   // throw an exception for invalid indices
   public void invalidIndices(int i, int j) {
+    System.out.println("test 11");
     if (i <= 0 || i > N) throw new IndexOutOfBoundsException("row index i out of bounds");
     if (j <= 0 || j > N) throw new IndexOutOfBoundsException("column index j out of bounds");
-  }
-
-  // test client (optional)
-  public static void main(String[] args) {
-    /* 
-     * call open(1, 1) and open(1, 2), and then to ensure that 
-     * the two corresponding entries are connected (using .connected() in WeightedQuickUnionUF). 
-     */
-    System.out.println("test");
-    Percolation p = new Percolation(10);
-    p.open(1, 1);
-    p.open(1, 2);
-    if(p.ali.connected(p.dimensionConverter(1, 1), p.dimensionConverter(1, 2))){
-      System.out.println("funzia");
-    }else
-      System.out.println("non funzia");
-    
-    if(p.ali.connected(p.dimensionConverter(1, 1), p.dimensionConverter(3, 2))){
-      System.out.println(" non funzia");
-    }else
-      System.out.println(" funzia");
+    System.out.println("completed test 11");
   }
 }
